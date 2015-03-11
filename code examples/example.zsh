14368d59411fa2278cb3e3911d49d3eed11110e3
rossPatton <me@rosspatton.com>
2015-03-11 14:28:48 -0700

    dev tools

---
+++ code examples/example.zsh
@@ -0,0 +1,13 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+
+alias dev='cd ~/Developer/'
+alias workspace='cd ~/Developer/workspace'
+alias wired="cd ~/Developer/workspace/wired-wp/"
+alias phoenix="cd ~/Developer/workspace/wired-wp/wp-content/themes/Phoenix/"
+alias euler="cd ~/Developer/workspace/projectEuler/"
+alias mobify="cd ~/Developer/workspace/mobifyjs-wired/"
+alias reload="source ~/.zshrc"
